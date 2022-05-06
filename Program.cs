e047fdd1f3b1183bccfb9f2aac120c9c82ba6e4d
Damian <103905478+DamianBosatta@users.noreply.github.com>
2022-05-05 23:36:48 -0300

    array, incersion, burbujeo, busqueda de elementos

---
+++ Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+namespace Corte_de_Control
+{
+    class Program
+    {
+        #region Challenge 3 punto 1
+        ////1 - Se tiene las notas de 4 parciales tomados a 30 alumnos ordenados en forma creciente por NRO DE ALUMNO
+        ////    .El nro de alumno es un valor entero entre 1 y 12000.Se desea ingresar un NRO DE ALUMNO y buscarlo por
+        ////    medio de la búsqueda dicotómica dentro del arreglo.
+        ////    Si se encuentra, dar el nro de alumno junto con las notas de los 4 parciales; sino exhibir cartel aclaratorio.
+
+        //public class Alumno
+        //{
+        //    public int nroAlumno;
+        //    public double nota1;
+        //    public double nota2;
+        //    public double nota3;
+        //    public double nota4;
+
+        //    public void cargaAlumno(Alumno[] arr)
+        //    {
+
+        //        for (int i = 0; i <= arr.Length; i++)
+        //        {
+
+        //            Console.WriteLine(" indique el numero del alumno");
+
+        //            arr[i].nroAlumno = int.Parse(Console.ReadLine());
+
+        //            Console.WriteLine(" iindique la nota del primer parcial");
+
+        //            arr[i].nota1 = int.Parse(Console.ReadLine());
+
+        //            Console.WriteLine(" indique la nota del segundo parcial");
+
+        //            arr[i].nota2 = int.Parse(Console.ReadLine());
+
+        //            Console.WriteLine(" indique la nota del tercer parcial");
+
+        //            arr[i].nota3 = int.Parse(Console.ReadLine());
+
+        //            Console.WriteLine(" indique la nota del cuarto parcial");
+
+        //            arr[i].nota4 = int.Parse(Console.ReadLine());
+
+
+
+
+
+
+        //        }
+
+        //        int aux;
+        //        for (int a = 1; a < arr.Length; a++) // ordenamiento por burbujeo
+        //            for (int b = arr.Length - 1; b >= a; b--)
+        //            {
+        //                if (arr[b - 1].nroAlumno > arr[b].nroAlumno)
+        //                {
+        //                    aux = arr[b - 1].nroAlumno;
+        //                    arr[b - 1].nroAlumno = arr[b].nroAlumno;
+        //                    arr[b].nroAlumno = aux;
+        //                }
+        //            }
+        //    }
+
+        //    public void Busqueda(Alumno[] arr)
+        //    {
+        //        int aux = 0;
+        //        Console.WriteLine(" ingrese el numero del alumno que busca ");
+        //        aux = int.Parse(Console.ReadLine());
+        //        bool flag = false;
+        //        for (int i = 0; i < arr.Length && flag == false; i++)
+        //        {
+
+        //            if (aux == arr[i].nroAlumno)
+        //            {
+        //                flag = true;
+        //                Console.WriteLine(" Alumno  Encontrado !!! en la posicion : " + i + " su numero de Alumno es : " + arr[i].nroAlumno);
+        //                Console.WriteLine(" Sus notas fueron : \n " + arr[i].nota1, arr[i].nota2, arr[i].nota3, arr[i].nota4);
+
+        //            }
+
+
+
+
+
+        //        }
+        //        if (flag == false)
+        //        {
+        //            Console.WriteLine(" no se encontro el alumno ");
+
+        //        }
+
+
+
+
+
+        //    }
+
+
+
+        //}
+
+
+
+        //static void Main(string[] args)
+        //{
+
+        //    Alumno alum = new Alumno();
+
+        //    Alumno[] Notas = new Alumno[30];
+
+        //    alum.cargaAlumno(Notas);
+
+        //    alum.Busqueda(Notas);
+
+
+
+
+
+        //}
+        #endregion
+
+
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+}
+    
+
